1444d43407bac5b657a82a23cbe2e3c9dd87a591
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-11-22 02:56:36 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -23,15 +23,6 @@ switch (userLang) {
         document.getElementById('we5').innerHTML = 'Marcamos la diferencia';
         document.getElementById('always').innerHTML = 'Siempre comprometidos con tu presupuesto y plazos.';
         document.getElementById('after').innerHTML = 'Despues de conocer tus necesidades estimamos cuanto tiempo y dinero nos va a llevar hacerlo realidad, te lo decimos y así será. No habrá retrasos ni aumentos de presupuesto durante todo el proceso.';
-        
-        document.getElementById('we6').innerHTML = 'Nos mantenemos en contacto.';
-        document.getElementById('you').innerHTML = 'Puedes contactar con nosotros en cualquier momento y resolveremos tus dudas lo antes posible. También te pediremos tus comentarios y expectativas sobre el proceso para ser lo más exactos posible para dar vida a tus ideas.';
-        document.getElementById('send').innerHTML = 'Lánzalo y comienza a generar ingresos.';
-        document.getElementById('finish').innerHTML = '¡Termina tu proyecto y comienza a vender y conseguir clientes!';
-        document.getElementById('getting').innerHTML = 'Ponerse en contacto es fácil';
-        document.getElementById('we7').innerHTML = 'Nos pondremos en contacto contigo lo antes posible. No te preocupes.';
-        document.getElementById('whats').innerHTML = `What's App Telegram o Signal:`;
-        document.getElementById('contact-2').innerHTML = `CONTACTO`;
         break;
 
     default:
