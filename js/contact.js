52fca6d23e97cfaec9a75675566c6fea37b10a51
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-12-12 11:29:58 +0000

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,7 @@
 const userLang = navigator.language || navigator.userLanguage; 
 switch (userLang) {
     case 'es-ES':
+        document.getElementById('home').innerHTML = 'INICIO';
         document.getElementById('contact-2').innerHTML = 'Contacto';
         document.getElementById('getting').innerHTML = 'Ponerse en contacto es fácil';
         document.getElementById('we7').innerHTML = 'Siempre te responderemos lo antes posible. No te preocupes.';
