39c7592840cd9cc2befc4e9714cb34399a28f6ce
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-12-29 12:21:40 +0000

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -7,6 +7,7 @@ switch (userLang) {
         document.getElementById('we7').innerHTML = 'Siempre te responderemos lo antes posible. No te preocupes.';
         document.getElementById('whats').innerHTML = `What's App Telegram o Signal:`;
         document.getElementById('contact-2').innerHTML = `CONTACTO`;
+        document.getElementById('call').innerHTML = `Aganda una llamada ahora`;
         break;
     default:
         break;
