ee93f901e5fc18640153af9d87bb7db78cb2e5fa
Gabriel Clemente <clementeaquipresente@yahoo.com>
2023-01-18 15:00:07 +0000

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,1 @@
-const userLang = navigator.language || navigator.userLanguage; 
-switch (userLang) {
-    case 'es-ES':
-        document.getElementById('home').innerHTML = 'INICIO';
-        document.getElementById('contact-2').innerHTML = 'Contacto';
-        document.getElementById('getting').innerHTML = 'Ponerse en contacto es fácil';
-        document.getElementById('we7').innerHTML = 'Siempre te responderemos lo antes posible. No te preocupes.';
-        document.getElementById('whats').innerHTML = `What's App Telegram o Signal:`;
-        document.getElementById('contact-2').innerHTML = `CONTACTO`;
-        document.getElementById('call').innerHTML = `Aganda una llamada ahora`;
-        break;
-    default:
-        break;
-}
+const userLang=navigator.language||navigator.userLanguage;"es-ES"===userLang&&(document.getElementById("home").innerHTML="INICIO",document.getElementById("contact-2").innerHTML="Contacto",document.getElementById("getting").innerHTML="Ponerse en contacto es f\xe1cil",document.getElementById("we7").innerHTML="Siempre te responderemos lo antes posible. No te preocupes.",document.getElementById("whats").innerHTML="What's App Telegram o Signal:",document.getElementById("contact-2").innerHTML="CONTACTO",document.getElementById("call").innerHTML="Aganda una llamada ahora");
