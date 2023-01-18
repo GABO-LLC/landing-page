7e2739a828dd383ce12bdccbe652d0d719d01d9b
Gabriel Clemente <clementeaquipresente@yahoo.com>
2023-01-18 14:06:03 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,4 +31,23 @@ switch (userLang) {
         break;
     default:
         break;
+   
+        
+ $('.brand-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  autoplay:true,
+  responsive:{
+    0:{
+      items:1
+    },
+    600:{
+      items:3
+    },
+    1000:{
+      items:5
+    }
+  }
+}) 
+
 }
